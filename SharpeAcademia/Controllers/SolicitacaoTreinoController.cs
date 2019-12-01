2a4897ba9e4ad7c60c6f1b95b69d120a81a72712
mahrselha <mahrselha@gmail.com>
2019-12-01 15:40:10 -0300

    Views

---
+++ SharpeAcademia/Controllers/SolicitacaoTreinoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using SharpeAcademia.Utils;
+
+namespace SharpeAcademia.Controllers
+{
+    public class SolicitacaoTreinoController : Controller { 
+
+    private readonly ProfessorDAO _professorDAO;
+    private readonly ClienteDAO _clienteDAO;
+    private readonly TreinoDAO _treinoDAO;
+    private readonly UtilsSession _utilsSession;
+
+    public SolicitacaoTreinoController(ProfessorDAO professorDAO,
+    ClienteDAO clienteDAO,
+    TreinoDAO treinoDAO,
+    UtilsSession utilsSession)
+    {
+        _professorDAO = professorDAO;
+        _clienteDAO = clienteDAO;
+        _treinoDAO = treinoDAO;
+        _utilsSession = utilsSession;
+    }
+    
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
